5420ea0cd00f2e0ab4c219bf2d3244ef38e27395
angaratosurion <angarato.surion@gmail.com>
2017-09-04 21:31:14 +0300

    gg
    gg

--- BucketBox/Devices/Monitor.cs
+++ BucketBox/Devices/Monitor.cs
@@ -7,6 +7,14 @@ using System.Text;
 
 namespace BucketBox.Devices
 {
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+    public struct PHYSICAL_MONITOR
+    {
+        public IntPtr hPhysicalMonitor;
+
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 128)]
+        public string szPhysicalMonitorDescription;
+    }
 
     [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
     public struct RAMP
@@ -18,16 +26,91 @@ namespace BucketBox.Devices
         [MarshalAs(UnmanagedType.ByValArray, SizeConst = 256)]
         public UInt16[] Blue;
     }
-    public  class Monitor
+    public  class Monitor : IDisposable
     {
         [DllImport("user32.dll")]
         public static extern IntPtr GetDC(IntPtr hWnd);
 
         [DllImport("gdi32.dll")]
         public static extern bool SetDeviceGammaRamp(IntPtr hDC, ref RAMP lpRamp);
-       
-       
-       
+        [DllImport("user32.dll", EntryPoint = "MonitorFromWindow")]
+        public static extern IntPtr MonitorFromWindow([In] IntPtr hwnd, uint dwFlags);
+
+        [DllImport("dxva2.dll", EntryPoint = "DestroyPhysicalMonitors")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool DestroyPhysicalMonitors(uint dwPhysicalMonitorArraySize, ref PHYSICAL_MONITOR[] pPhysicalMonitorArray);
+
+        [DllImport("dxva2.dll", EntryPoint = "GetNumberOfPhysicalMonitorsFromHMONITOR")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetNumberOfPhysicalMonitorsFromHMONITOR(IntPtr hMonitor, ref uint pdwNumberOfPhysicalMonitors);
+
+        [DllImport("dxva2.dll", EntryPoint = "GetPhysicalMonitorsFromHMONITOR")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetPhysicalMonitorsFromHMONITOR(IntPtr hMonitor, uint dwPhysicalMonitorArraySize, [Out] PHYSICAL_MONITOR[] pPhysicalMonitorArray);
+
+        [DllImport("dxva2.dll", EntryPoint = "GetMonitorBrightness")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetMonitorBrightness(IntPtr handle, ref uint minimumBrightness, ref uint currentBrightness, ref uint maxBrightness);
+
+        [DllImport("dxva2.dll", EntryPoint = "SetMonitorBrightness")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SetMonitorBrightness(IntPtr handle, uint newBrightness);
+
+        private uint _physicalMonitorsCount = 0;
+        private PHYSICAL_MONITOR[] _physicalMonitorArray;
+
+        private IntPtr _firstMonitorHandle;
+
+        private uint _minValue = 0;
+        private uint _maxValue = 0;
+        private uint _currentValue = 0;
+        public Monitor(IntPtr windowHandle)
+        {
+            uint dwFlags = 0u;
+            IntPtr ptr = MonitorFromWindow(windowHandle, dwFlags);
+            if (!GetNumberOfPhysicalMonitorsFromHMONITOR(ptr, ref _physicalMonitorsCount))
+            {
+                throw new Exception("Cannot get monitor count!");
+            }
+            _physicalMonitorArray = new PHYSICAL_MONITOR[_physicalMonitorsCount];
+
+            if (!GetPhysicalMonitorsFromHMONITOR(ptr, _physicalMonitorsCount, _physicalMonitorArray))
+            {
+                throw new Exception("Cannot get phisical monitor handle!");
+            }
+            _firstMonitorHandle = _physicalMonitorArray[0].hPhysicalMonitor;
+
+            if (!GetMonitorBrightness(_firstMonitorHandle, ref _minValue, ref _currentValue, ref _maxValue))
+            {
+                throw new Exception("Cannot get monitor brightness!");
+            }
+        }
+
+        public void SetBrightness(int newValue)
+        {
+            newValue = Math.Min(newValue, Math.Max(0, newValue));
+            _currentValue = (_maxValue - _minValue) * (uint)newValue / 100u + _minValue;
+            SetMonitorBrightness(_firstMonitorHandle, _currentValue);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (_physicalMonitorsCount > 0)
+                {
+                    DestroyPhysicalMonitors(_physicalMonitorsCount, ref _physicalMonitorArray);
+                }
+            }
+        }
+
+
         public  void SetGamma(int gamma)
         {
             if (gamma <= 256 && gamma >= 1)
--- BucketBox/Properties/AssemblyInfo.cs
+++ BucketBox/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BucketBox")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright ©  2013 -2017 Paris Koutsioukis")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
