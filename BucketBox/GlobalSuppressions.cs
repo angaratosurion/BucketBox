39277a32f0670a9ac6d553e20fa9859aed053991
angaratosurion <angarato.surion@gmail.com>
2020-10-21 02:26:58 +0300

    d
    added wayt to get the profiles of monitor

---
+++ BucketBox/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Naming", "CA1712:Do not prefix enum values with type name", Justification = "<Pending>", Scope = "member", Target = "~F:BucketBox.Devices.MonitorProfile.WCS_PROFILE_MANAGEMENT_SCOPE.WCS_PROFILE_MANAGEMENT_SCOPE_CURRENT_USER")]
--- BucketBox/Properties/AssemblyInfo.cs
+++ BucketBox/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BucketBox")]
-[assembly: AssemblyCopyright("Copyright ©  2013 -2017 Paris Koutsioukis")]
+[assembly: AssemblyCopyright("Copyright ©  2020 Paris Koutsioukis")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.9.0")]
-[assembly: AssemblyFileVersion("0.0.9.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
